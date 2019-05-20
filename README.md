982c97230eed08252c12f8b003ba6e9221f7fe96
qngolg@126.com <qngolg@126.com>
2019-05-20 20:05:46 +0800

    [N/A] add docker file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Docker-mysql-demo
+https://www.jianshu.com/p/75adf667f61a#Docker-mysql-demo
 
 获取docker镜像
 `docker pull mysql`
@@ -7,9 +7,9 @@
 `docker images`
 
 安装运行docker mysql镜像 第一个3306 映射的本地port 第二个3306 docker容器的port
-``sudo docker run --name first-mysql -p 3306:3306 -e MYSQL\_ROOT\_PASSWORD=123456 -d mysql``
-
+``sudo docker run --name first-mysql -p 3306:3306 -e MYSQL\_ROOT\_PASSWORD=123456 -d mysql 08dc7169369f``
 查看运行状态
+
 ``docker ps``
 
 使用InteilliJ IDEA 的database连接mysql
@@ -17,5 +17,5 @@
 设置用户名密码
 root  123456
 
-
-
+##启动已经停止的docker容器
+``docker start first-mysql``
--- src/main/java/com/qngolg/dockermysqldemo/DockerMysqlDemoApplication.java
+++ src/main/java/com/qngolg/dockermysqldemo/DockerMysqlDemoApplication.java
@@ -2,12 +2,20 @@ package com.qngolg.dockermysqldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerMysqlDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerMysqlDemoApplication.class, args);
+    }
+
+    @GetMapping("/test/test")
+    public String sayHello(){
+        return "jenkins + spring boot + docker 运行成功了";
     }
 
 }
